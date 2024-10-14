8ddba46091bbf0165869dee7f1a611eba27de5ee
Mohammad Tahmasebi <mohammad.tahmasebi@students.codeinstitute.net>
2024-10-14 14:20:31 +0100

    Update main.js
    modify the parameter referenced by the xhr.open function

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const URL = 'https://ci-swapi.herokapp.com/api/';
 
 function getData(type, data) {
   var xhr = new XMLHttpRequest();
-  xhr.open('GET', URL + type);
+  xhr.open('GET', URL + type + '/');
   xhr.send();
   xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 400) {
