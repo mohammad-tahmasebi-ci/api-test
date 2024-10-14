caac29d4cf23f97ea88281c76af1cce40422ff55
Mohammad Tahmasebi <mohammad.tahmasebi@students.codeinstitute.net>
2024-10-14 12:59:14 +0100

    Update main.js
    add missing semicolon

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ xhr.onreadystatechange = function() {
   if (this.readystate == 4 && this.status == 400) {
     document.getElementById('data') = this.responseText;
 }
-}
+};
 xhr.open('GET', 'https://ci-swapi.herokuapp.com/api/');
 xhr.send();
