0f81b86c1ba8ab3a610b3132c3c7f694b4106c07
Mohammad Tahmasebi <mohammad.tahmasebi@students.codeinstitute.net>
2024-10-14 13:01:19 +0100

    Update main.js
    modify the code

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 var xhr = new XMLHttpRequest();
 
 xhr.onreadystatechange = function() {
-  if (this.readystate == 4 && this.status == 400) {
-    document.getElementById('data') = this.responseText;
+  if (this.readyState == 4 && this.status == 400) {
+    document.getElementById('data').innerHTML = this.responseText;
 }
 };
 xhr.open('GET', 'https://ci-swapi.herokuapp.com/api/');
